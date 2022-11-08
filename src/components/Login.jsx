da496a88d0668bbfa58be424a4224c96f5b9de6e
= <=>
2022-11-08 13:01:10 -0600

    se creó el componente Login

---
+++ src/components/Login.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import '../css/main.css'
+import '../css/util.css'
+
+const Login = () => {
+  return (
+    <div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 p-t-85 p-b-20">
+				<form class="login100-form validate-form">
+					<span class="login100-form-title p-b-70">
+						Welcome to MyBandTour
+					</span>
+					<div class="wrap-input100 validate-input m-t-85 m-b-35" data-validate="Enter email">
+						<input class="input100" type="text" name="email"></input>
+						<span class="focus-input100" data-placeholder="Email"></span>
+					</div>
+					<div class="wrap-input100 validate-input m-b-50" data-validate="Enter password">
+						<input class="input100" type="password" name="pass"></input>
+						<span class="focus-input100" data-placeholder="Password"></span>
+					</div>
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+					<ul class="login-more p-t-190">
+						<li>
+							<span class="txt1"> Don’t have an account?</span>
+							<a href="#" class="txt2">Sign up</a>
+						</li>
+					</ul>
+				</form>
+			</div>
+		</div>
+	</div>	
+  );
+}
+
+export default Login
