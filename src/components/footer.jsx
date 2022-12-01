9ba10b91a2690dcd7a5baefff1232952c7b5e83a
= <=>
2022-12-01 11:23:03 -0600

    se añadió el componente de footer, mas la estructura de la pantalla home y main

---
+++ src/components/footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const footer = () => {
+    return (
+        <div>
+            <footer>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <p>Copyright &copy; 2022 MyBandTour - Diseñado por Estudiantes de la UH</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default footer
