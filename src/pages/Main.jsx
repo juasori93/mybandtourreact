479e90268a6d45103476e1dabba3ef17a2b42d31
= <=>
2022-12-07 14:03:43 -0600

    test

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { HashRouter, NavLink } from 'react-router-dom'
 import '../css/style.css'
 import '../css/templatemo-training-studio.css'
 import Header from '../components/Header'
