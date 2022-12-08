0e943ec15e40f4a97acf6d27116eb9e44f3f13e7
Juan Antonio Solano Rossi <juan.solano@uhispano.ac.cr>
2022-12-07 19:50:35 -0600

    Cambio de nombre footer

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,7 +3,7 @@ import '../css/style.css';
 import '../css/templatemo-training-studio.css';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
-import Footer from '../components/footer';
+import Footer from '../components/Footer';
 import Events from '../components/Events';
 
 const Main = () => {
